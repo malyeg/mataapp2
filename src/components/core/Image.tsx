87a0d17b91ce731d89d9ffe1b7ef0670c393ef2e
malyeg <maly.egp@gmail.com>
2021-08-11 13:30:50 +0200

    support us

--- src/components/core/Image.tsx
+++ src/components/core/Image.tsx
@@ -1,24 +1,34 @@
 import React, {FC} from 'react';
 import {ImageStyle, StyleProp, StyleSheet} from 'react-native';
-import FastImage, {ResizeMode} from 'react-native-fast-image';
+import FastImage, {ResizeMode, Source} from 'react-native-fast-image';
 interface ImageProps {
-  uri: string;
+  uri?: string;
+  source?: Source;
   cache?: 'immutable' | 'web' | 'cacheOnly';
   style?: StyleProp<ImageStyle>;
   resizeMode?: ResizeMode;
 }
 
-const Image = ({style, cache, uri, resizeMode, ...props}: ImageProps) => {
+const Image = ({
+  style,
+  cache,
+  source,
+  uri,
+  resizeMode,
+  ...props
+}: ImageProps) => {
   const styleList = [styles.image, style];
   return (
     <FastImage
       {...props}
       style={styleList}
-      source={{
-        uri,
-        priority: FastImage.priority.normal,
-        cache: cache ?? FastImage.cacheControl.immutable,
-      }}
+      source={
+        source ?? {
+          uri,
+          priority: FastImage.priority.normal,
+          cache: cache ?? FastImage.cacheControl.immutable,
+        }
+      }
       resizeMode={resizeMode ?? FastImage.resizeMode.cover}
     />
   );
--- src/screens/SupportUsScreen.tsx
+++ src/screens/SupportUsScreen.tsx
@@ -1,20 +1,57 @@
 import React from 'react';
 import {Linking, StyleSheet, View} from 'react-native';
-import {Button, Image} from '../components/core';
-import {payment} from '../config/constants';
+import {Button, Image, Screen, Text} from '../components/core';
+import {payment, screens} from '../config/constants';
+import useLocale from '../hooks/useLocale';
+import theme from '../styles/theme';
 
 const SupportUsScreen = () => {
+  const {t} = useLocale(screens.SUPPORT_US);
   return (
-    <View>
-      {/* <Image uri="" /> */}
-      <Button
-        title="Buy me a coffee"
-        onPress={() => Linking.openURL(payment.SUPPORT_US_URL)}
-      />
-    </View>
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <Image
+          source={require('../assets/images/support-us.png')}
+          style={styles.image}
+        />
+        <Text style={styles.content}>{t('content')}</Text>
+        <Button
+          title="Support"
+          style={styles.button}
+          textStyle={styles.buttonText}
+          onPress={() => Linking.openURL(payment.SUPPORT_US_URL)}
+        />
+      </View>
+    </Screen>
   );
 };
 
 export default SupportUsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    justifyContent: 'center',
+    // alignItems: 'center',
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: '50%',
+  },
+  container: {
+    flex: 0.75,
+    justifyContent: 'space-evenly',
+    // alignItems: 'center',
+  },
+  content: {
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: theme.colors.green,
+    // borderColor: theme.colors.grey,
+    // borderWidth: 2,
+  },
+  buttonText: {
+    color: theme.colors.white,
+  },
+});
